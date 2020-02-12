3a084265868c08bf31696c2c2cc0c121052e398d
Rainer Simon <rainer.simon@ait.ac.at>
2020-02-12 16:44:06 +0100

    First working conversion utility

--- convert.py
+++ convert.py
@@ -1,4 +1,5 @@
 import csv
+import glob
 import io
 import json
 import zipfile
@@ -31,9 +32,21 @@ class Feature:
 
     return json.dumps(feature)
 
-with zipfile.ZipFile('./downloads/AT.zip', 'r') as archive:
-  with archive.open('AT.txt') as f:
-    reader = csv.reader(io.TextIOWrapper(f), delimiter='\t')
-    for row in reader:
-      feature = Feature(row)
-      print(feature.to_json())
+def convert_zip(ccode):
+  with open('./output/geonames_all.lpf.jsonl', 'a') as outfile:
+    with zipfile.ZipFile(f'./downloads/{ccode}.zip', 'r') as archive:
+      with archive.open(f'{ccode}.txt') as f:
+        reader = csv.reader(io.TextIOWrapper(f), delimiter='\t')
+        for row in reader:
+          feature = Feature(row)
+          outfile.write(f'{feature.to_json()}\n')
+
+# TODO delete outfile
+
+zipfiles = [f for f in glob.glob('./downloads/*.zip')]
+for f in zipfiles:
+  ccode = f[f.rfind('/') + 1 : -4]  
+
+  print(f'Converting file {f}')
+  convert_zip(ccode)
+
